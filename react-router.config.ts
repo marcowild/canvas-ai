51b97ef05fbb2438b5612dc2832e43de5a39dc62
marcowild <marco@wild.as>
2025-11-17 14:48:13 +0100

    fix

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,6 +4,11 @@ export default {
   ssr: true,
   serverBuildFile: "index.js",
   async prerender() {
+    // Disable prerendering if Basic Auth is enabled
+    // since authenticated routes can't be statically rendered
+    if (process.env.BASIC_AUTH_USERNAME && process.env.BASIC_AUTH_PASSWORD) {
+      return [];
+    }
     return ["/"];
   },
 } satisfies Config;
